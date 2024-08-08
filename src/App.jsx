79fd1b14323f9dbaeccbab37f55c00634c5b1624
Ravina Hudda <105847305+ravina-hudda@users.noreply.github.com>
2024-08-08 14:10:01 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useCallback } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength]= useState(8)
+  const [numberAllowed, setNumberAllowed]= useState(false)
+  const [charAllowed, setCharAllowed]= useState(false)
+  const [password, setPassword]=useState("")
 
+  const passwordGenerator=useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str+="0123456789"
+    if(charAllowed) str+="!@#$%^&*_+-={}[]~`"
+    for(leti=1;i<=length;i++){
+      pass+= str.charAt(Math.floor(Math.random*str.length()+1))
+      
+    }
+     setPassword(pass)
+  },[numberAllowed,charAllowed,length,setPassword])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Password Generator</h1>
+
+      <div className='container'> test </div>
     </>
   )
 }
