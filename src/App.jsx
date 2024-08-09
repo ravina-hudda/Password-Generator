e66aaed57269c753f6a3882b64aa6b66584325cd
Ravina Hudda <105847305+ravina-hudda@users.noreply.github.com>
2024-08-09 05:52:33 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect,useRef} from 'react'
 
 import './App.css'
 
@@ -8,22 +8,51 @@ function App() {
   const [charAllowed, setCharAllowed]= useState(false)
   const [password, setPassword]=useState("")
 
+  const passwordRef= useRef(null)
+  
   const passwordGenerator=useCallback(()=>{
     let pass=""
     let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if(numberAllowed) str+="0123456789"
     if(charAllowed) str+="!@#$%^&*_+-={}[]~`"
-    for(leti=1;i<=length;i++){
-      pass+= str.charAt(Math.floor(Math.random*str.length()+1))
+    for(let i=1;i<=length;i++){
+      pass+= str.charAt( Math.floor(Math.random()*str.length+1))
       
     }
      setPassword(pass)
   },[numberAllowed,charAllowed,length,setPassword])
+
+  const copyPasswordToClip = useCallback(()=>{
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  },[password])
+  
+  useEffect(()=>{passwordGenerator()},[length,numberAllowed,charAllowed,passwordGenerator])
   return (
     <>
       <h1>Password Generator</h1>
-
-      <div className='container'> test </div>
+      
+      <div className='container'>
+        <div className='passContainer'>
+          <input type="text" value={password} className='passInput' placeholder='Password' ref={passwordRef}/>
+          <button className='btn' onClick={copyPasswordToClip}>copy</button>
+        </div>
+        <div className='depContainer'>
+          <div className='subDepContainer'>
+            <input className='inputs' type="range" value={length} min={6} max={50} onChange={(e)=>{setLength(e.target.value)}}/>
+            <label>Length : {length}</label>
+          </div>
+          <div className='subDepContainer'>
+            <input className='inputs' type="checkbox" value={numberAllowed} onChange={()=>{setNumberAllowed((prev)=>!prev)}}/>
+            <label > Numbers</label>
+          </div>
+          <div className='subDepContainer'>
+            <input className='inputs' type="checkbox" value={charAllowed} onChange={()=>{setCharAllowed((prev)=>!prev)}}/>
+            <label >Special Characters</label>
+          </div>
+        </div>
+        
+      </div>
     </>
   )
 }
